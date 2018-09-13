e6ca31878c4824250d5b2275f571cddf653447f1
Xavier Duncan <xduncan@pm.me>
2018-09-13 10:39:39 -0400

    add header component

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const h = React.createElement;
 
+// dumb, presentational components (pure functions, stateless)
+let Header = () =>
+    h('div', {}, [
+        h('h1', {}, [
+            '🔥 SimpleSocial'
+        ])
+    ]);
+
 let Homepage = () =>
-    h('h1', {}, [
-        'Homepage works!'
+    h('div', {}, [
+        h(Header)
     ]);
 
 ReactDOM.render(h(Homepage), document.querySelector('.react-root'));
